c504de516049e38b3d7a84de0a2d00be198b32a6
DIDI <124243299+yd1064@users.noreply.github.com>
2023-02-03 15:07:30 +0900

    Update JH.java

--- DEF/src/JH.java
+++ DEF/src/JH.java
@@ -1,8 +1,12 @@
+import java.util.Scanner; 
 
 public class JH {
 
 	public JH() {
 		// TODO Auto-generated constructor stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("정수입력");
+		int n = scan.nextInt();
 	}
 
 }
